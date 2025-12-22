49e95a5c2f79985230e28eb4ef483921530318d3
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-22 15:00:59 +0530

    fix errors

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,17 +1,19 @@
-import express from "express"
-import isAuthenticated from "../middlewares/auth.js"
-import { getAdminJobs, getAllJobs, jobById, postJob } from "../controllers/job.controller.js"
+import express from "express";
+import isAuthenticated from "../middlewares/auth.js";
+import {
+  getAdminJobs,
+  getAllJobs,
+  jobById,
+  postJob,
+} from "../controllers/job.controller.js";
 
-const jobRouter = express.Router()
-
-
+const jobRouter = express.Router();
 
 //routes
 
-jobRouter.post("/post",isAuthenticated,postJob)
-jobRouter.get("/get",getAllJobs)
-jobRouter.get("/get/:id",isAuthenticated,jobById)
-jobRouter.get("/getadminjobs",isAuthenticated,getAdminJobs)
+jobRouter.post("/post", isAuthenticated, postJob);
+jobRouter.get("/get", getAllJobs);
+jobRouter.get("/get/:id", isAuthenticated, jobById);
+jobRouter.get("/getadminjobs", isAuthenticated, getAdminJobs);
 
-
-export default jobRouter
+export default jobRouter;
