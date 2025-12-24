a1ad2245c8e856c434afd2833ca82c973edaa4b5
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-24 20:17:50 +0530

    add latest job component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
 import CategorySection from "./CategorySection";
+import LatestJobs from "./LatestJobs";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <CategorySection/>
+      <CategorySection />
+      <LatestJobs />
     </div>
   );
 };
---
+++ frontend/src/components/LatestJobs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LatestJobs = () => {
+  return (
+    <div className="max-w-7xl mx-auto my-20">
+      <h1 className="text-4xl font-bold">
+        <span className="text-[#6a38c2]">Latest & Top </span>
+        Job Openings
+      </h1>
+
+      {/* cards */}
+    </div>
+  );
+};
+
+export default LatestJobs;
