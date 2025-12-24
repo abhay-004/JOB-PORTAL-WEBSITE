eb42ec816a230858d51beaf4a96e4dc5b6652d37
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-24 20:12:34 +0530

    add category section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
 import HeroSection from "./HeroSection";
+import CategorySection from "./CategorySection";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <CategorySection/>
     </div>
   );
 };
