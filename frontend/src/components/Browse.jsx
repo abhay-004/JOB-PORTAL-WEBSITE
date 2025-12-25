24f60b71325b4dd3a0a6833a12ea1235835af02c
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-25 16:15:01 +0530

    create browse page

---
+++ frontend/src/components/Browse.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "./shared/Navbar";
+import JobCard from "./JobCard";
+
+const Browse = () => {
+  const randomJobs = [1, 2, 3];
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto my-10">
+        <h1 className="font-bold text-lg my-10">
+          Search Results ({randomJobs.length})
+        </h1>
+        <div className="grid grid-cols-3 gap-4 mt-5">
+          {randomJobs.map((item, index) => (
+            <JobCard />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Browse;
