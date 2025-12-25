f4201020270cd19cfd913448bc6897decdc7ae77
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-25 13:14:00 +0530

    crete jobs page

---
+++ frontend/src/components/JobCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JobCard = () => {
+  return (
+    <div>
+      job
+    </div>
+  )
+}
+
+export default JobCard
--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./shared/Navbar";
+import FilterCard from "./FilterCard";
+import JobCard from "./JobCard";
 
+const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 const Jobs = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Navbar />
+      <div className="max-w-7xl mx-auto mt-5">
+        <div className="flex gap-5">
+          <div className="w-[20%]">
+            <FilterCard />
+          </div>
 
-export default Jobs
+          {jobsArray.length <= 0 ? (
+            <span>Job Not Found</span>
+          ) : (
+            <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
+              <div className="grid grid-cols-3 gap-4">
+                {jobsArray.map(() => (
+                  <div>
+                    <JobCard />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jobs;
