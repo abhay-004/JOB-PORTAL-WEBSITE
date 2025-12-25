c21f2d92aff6ad5b8775a2c116970b00b394147f
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-25 12:37:52 +0530

    add latestjobsCards

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import LatestJobsCards from "./LatestJobsCards";
 
+const randomJobs = [1, 2, 3, 4, 5, 6, 7, 8];
 const LatestJobs = () => {
   return (
     <div className="max-w-7xl mx-auto my-20">
@@ -9,6 +11,11 @@ const LatestJobs = () => {
       </h1>
 
       {/* cards */}
+      <div className="grid grid-cols-3 gap-4 my-5">
+        {randomJobs.slice(0, 6).map((item, index) => (
+          <LatestJobsCards />
+        ))}
+      </div>
     </div>
   );
 };
