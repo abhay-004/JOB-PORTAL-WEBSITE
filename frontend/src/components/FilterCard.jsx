9215d268e2723ea7711cd4f7575846d3081fd569
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-25 15:34:49 +0530

    create filtercard page

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
 
+const filterData = [
+  {
+    filterType: "Location",
+    array: ["Delhi", "Noida", "Agra", "Pune", "Mumbai", "Hyderabad"],
+  },
+  {
+    filterType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "FullStack Developer"],
+  },
+  {
+    filterType: "Salary",
+    array: ["0-40k", "42-1lakh", "1lakh to 5lakh"],
+  },
+];
 const FilterCard = () => {
   return (
-    <div>
-      filter
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
+      <hr className="mt-3" />
+      <RadioGroup>
+        {filterData.map((data, index) => (
+          <div key={index}>
+            <h1 className="font-bold text-lg">{data.filterType}</h1>
+            {data.array.map((item, index) => (
+              <div key={index} className="flex items-center space-x-2 my-2">
+                <RadioGroupItem value={item} />
+                <Label>{item}</Label>
+              </div>
+            ))}
+          </div>
+        ))}
+      </RadioGroup>
     </div>
-  )
-}
+  );
+};
 
-export default FilterCard
+export default FilterCard;
