e750bbfb2bf7c379c49a290562ed3261c9050a32
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-25 12:55:45 +0530

    link routes and add job route

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -10,17 +10,23 @@ const Navbar = () => {
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl">
         <div>
-          <h1 className="text-2xl font-bold">
-            Job <span className="text-[#f83002]">Portal</span>
-          </h1>
+          <Link to={"/"}>
+            <h1 className="text-2xl font-bold">
+              Job <span className="text-[#f83002]">Portal</span>
+            </h1>
+          </Link>
         </div>
 
         {/* Right side  */}
 
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li>Home</li>
-            <li>Job</li>
+            <Link to={"/"}>
+              <li>Home</li>
+            </Link>
+            <Link to={"/jobs"}>
+              <li>Job</li>
+            </Link>
             <li>Browse</li>
           </ul>
 
