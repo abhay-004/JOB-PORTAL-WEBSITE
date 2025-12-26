6822f13f18a619a1ebe1a9d0aa2a6773d5e31298
Abhay Chauhan <chauhanabhay88688@gmail.com>
2025-12-26 17:35:41 +0530

    add job description route

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -3,8 +3,11 @@ import { Button } from "./ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const JobCard = () => {
+  const navigate = useNavigate();
+  const jobId = "asdfhalugfaldsh";
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex items-center justify-between">
@@ -47,7 +50,11 @@ const JobCard = () => {
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
-        <Button variant="outline" className={"cursor-pointer"}>
+        <Button
+          onClick={() => navigate(`/description/${jobId}`)}
+          variant="outline"
+          className={"cursor-pointer"}
+        >
           Details
         </Button>
         <Button className={"bg-[#7209b7] cursor-pointer"}>
